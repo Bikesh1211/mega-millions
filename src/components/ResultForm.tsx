07d1ef16d6f14b983339dc3915940b1c39f70d4e
arb12345 <ajayranabhat19@gmail.com>
2023-08-15 10:30:21 +0545

    result from input fix

--- src/components/ResultForm.tsx
+++ src/components/ResultForm.tsx
@@ -1,18 +1,18 @@
-"use client";
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {
   FormControl,
   InputLabel,
@@ -23,19 +23,19 @@ import {
   Modal,
   Fade,
   IconButton,
-} from "@mui/material";
-import Image from "next/image";
-import styles from "../styles/image.module.css";
-import { Form, FormikProvider, useFormik } from "formik";
-import { useGetRequest } from "@/hooks/useApi";
-import axios from "axios";
-import LotteryResult from "./LotteryResult";
-import CloseIcon from "@mui/icons-material/Close";
+} from '@mui/material';
+import Image from 'next/image';
+import styles from '../styles/image.module.css';
+import { Form, FormikProvider, useFormik } from 'formik';
+import { useGetRequest } from '@/hooks/useApi';
+import axios from 'axios';
+import LotteryResult from './LotteryResult';
+import CloseIcon from '@mui/icons-material/Close';
 
 const defaultTheme = createTheme();
 
 export default function ResultForm() {
-  const [category, setCategory] = React.useState("");
+  const [category, setCategory] = React.useState('');
   const [isLoading, setIsLoading] = React.useState(false);
   const [isResultVisible, setIsResultVisible] = React.useState(false);
   const handleChange = (event: SelectChangeEvent) => {
@@ -43,34 +43,34 @@ export default function ResultForm() {
   };
   const [lotteryData, setLotteryData] = React.useState<any>(null);
   const lotteryNumber = [
-    { id: 1, name: "num1" },
-    { id: 2, name: "num2" },
-    { id: 3, name: "num3" },
-    { id: 4, name: "num4" },
-    { id: 5, name: "num5" },
-    { id: 6, name: "num6" },
+    { id: 1, name: 'num1' },
+    { id: 2, name: 'num2' },
+    { id: 3, name: 'num3' },
+    { id: 4, name: 'num4' },
+    { id: 5, name: 'num5' },
+    { id: 6, name: 'num6' },
   ];
 
   const formik = useFormik({
     initialValues: {
-      num1: "",
-      num2: "",
-      num3: "",
-      num4: "",
-      num5: "",
-      num6: "",
-      category: "MegaMillion",
+      num1: '',
+      num2: '',
+      num3: '',
+      num4: '',
+      num5: '',
+      num6: '',
+      category: 'MegaMillion',
     },
     onSubmit: async (values: any) => {
       setIsLoading(true);
 
       try {
-        if (values.category === "PowerBall") {
+        if (values.category === 'PowerBall') {
           const apiUrl = `https://helpful-shorts-pig.cyclic.app/api/lottery/powerball?userNumber=${values.num1},${values.num2},${values.num3},${values.num4},${values.num5},${values.num6}`;
           const response = await axios.post(apiUrl);
           setLotteryData(response.data);
           setIsResultVisible(true);
-        } else if (values.category === "MegaMillion") {
+        } else if (values.category === 'MegaMillion') {
           const apiUrl = `https://helpful-shorts-pig.cyclic.app/api/lottery/megamillion?userNumber=${values.num1},${values.num2},${values.num3},${values.num4},${values.num5},${values.num6}`;
           const response = await axios.post(apiUrl);
           setLotteryData(response.data);
@@ -117,19 +117,19 @@ export default function ResultForm() {
               sx={{
                 mt: 1,
                 gap: 2,
-                background: "rgba(255, 255, 255, 0.2)",
-                borderRadius: "16px",
-                boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-                backdropFilter: "blur(5px)",
-                WebkitBackdropFilter: "blur(5px)",
-                border: "1px solid rgba(255, 255, 255, 0.3)",
+                background: 'rgba(255, 255, 255, 0.2)',
+                borderRadius: '16px',
+                boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+                backdropFilter: 'blur(5px)',
+                WebkitBackdropFilter: 'blur(5px)',
+                border: '1px solid rgba(255, 255, 255, 0.3)',
                 p: 5,
                 minHeight: 500,
               }}
             >
               <Image
-                alt="logo"
-                src="/logo.svg.png"
+                alt='logo'
+                src='/logo.svg.png'
                 width={300}
                 height={150}
                 className={styles.image}
@@ -137,25 +137,25 @@ export default function ResultForm() {
               <Box>
                 <FormControl fullWidth>
                   <InputLabel
-                    id="demo-simple-select-label"
-                    sx={{ color: "white" }}
+                    id='demo-simple-select-label'
+                    sx={{ color: 'white' }}
                   >
                     Select Category
                   </InputLabel>
                   <Select
-                    id="demo-simple-select"
+                    id='demo-simple-select'
                     value={formik.values.category}
-                    label="Select Category"
+                    label='Select Category'
                     onChange={(event: any) => {
                       handleChange(event);
-                      formik.setFieldValue("category", event.target.value);
+                      formik.setFieldValue('category', event.target.value);
                     }}
-                    sx={{ color: "white" }}
+                    sx={{ color: 'white' }}
                   >
-                    <MenuItem value={"PowerBall"} color="white">
+                    <MenuItem value={'PowerBall'} color='white'>
                       PowerBall
                     </MenuItem>
-                    <MenuItem value={"MegaMillion"} color="white">
+                    <MenuItem value={'MegaMillion'} color='white'>
                       MegaMillion
                     </MenuItem>
                   </Select>
@@ -163,12 +163,12 @@ export default function ResultForm() {
               </Box>
               <Box>
                 <Typography
-                  color="initial"
-                  sx={{ fontSize: 15, fontWeight: 500, color: "white" }}
+                  color='initial'
+                  sx={{ fontSize: 15, fontWeight: 500, color: 'white' }}
                 >
                   Enter Your Lottery Number
                 </Typography>
-                <Stack direction={"row"}>
+                <Stack direction={'row'}>
                   {lotteryNumber.map((each, index) => {
                     return (
                       <TextField
@@ -176,15 +176,15 @@ export default function ResultForm() {
                         inputRef={inputRefs[index]}
                         required
                         fullWidth
-                        type="number"
+                        type='number'
                         id={`number${index}`}
                         sx={{
                           borderRadius: 2,
-                          color: "white",
+                          color: 'white',
                         }}
                         inputProps={{
-                          className: "custom-number-input",
-                          style: { fontSize: 10, color: "white" },
+                          className: 'custom-number-input',
+                          style: { fontSize: 9, color: 'white' },
                         }}
                         onChange={(event: any) =>
                           handleInputChange(event, index)
@@ -194,27 +194,27 @@ export default function ResultForm() {
                   })}
                 </Stack>
               </Box>
-              <Stack direction={"column"} sx={{ gap: 1 }}>
+              <Stack direction={'column'} sx={{ gap: 1 }}>
                 <Button
-                  type="submit"
+                  type='submit'
                   fullWidth
-                  variant="contained"
+                  variant='contained'
                   sx={{
                     borderRadius: 2,
-                    backgroundColor: "#302F7B",
-                    textTransform: "capitalize",
+                    backgroundColor: '#302F7B',
+                    textTransform: 'capitalize',
                   }}
                   disabled={isLoading}
                 >
-                  {isLoading ? "Checking..." : "Check Result"}
+                  {isLoading ? 'Checking...' : 'Check Result'}
                 </Button>
                 <Button
                   fullWidth
-                  variant="contained"
+                  variant='contained'
                   sx={{
                     borderRadius: 2,
-                    backgroundColor: "#302F7B",
-                    textTransform: "capitalize",
+                    backgroundColor: '#302F7B',
+                    textTransform: 'capitalize',
                   }}
                 >
                   Scan Ticket
@@ -226,32 +226,32 @@ export default function ResultForm() {
               onClose={closeModal}
               closeAfterTransition
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
               <Fade in={isResultVisible}>
                 <div
                   style={{
-                    backgroundColor: "#fff",
-                    padding: "20px",
-                    borderRadius: "8px",
-                    maxWidth: "350px",
-                    width: "100%",
-                    textAlign: "center",
-                    position: "relative",
+                    backgroundColor: '#fff',
+                    padding: '20px',
+                    borderRadius: '8px',
+                    maxWidth: '350px',
+                    width: '100%',
+                    textAlign: 'center',
+                    position: 'relative',
                   }}
                 >
                   <IconButton
-                    aria-label="close"
+                    aria-label='close'
                     onClick={closeModal}
-                    sx={{ position: "absolute", top: 0, right: 0 }}
+                    sx={{ position: 'absolute', top: 0, right: 0 }}
                   >
                     <CloseIcon />
                   </IconButton>
                   <LotteryResult
-                    prizeHeadings={lotteryData?.prize?.headings || ""}
+                    prizeHeadings={lotteryData?.prize?.headings || ''}
                     winningNumbers={lotteryData?.winningNumbers || []}
                     matchedNumbers={lotteryData?.userNumbers || []}
                   />
